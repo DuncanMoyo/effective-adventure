f865b8d3871414e51e5091efa51b78f08089ad94
Duncan Moyo <duncanfmoyo@gmail.com>
2021-05-20 10:12:02 +0200

    Managing the MediaQuery object

--- lib/main.dart
+++ lib/main.dart
@@ -96,7 +96,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    final isLandscape = MediaQuery.of(context).orientation == Orientation.landscape;
+    final mediaQuery = MediaQuery.of(context);
+    final isLandscape = mediaQuery.orientation == Orientation.landscape;
     final appBar = AppBar(
       title: Text(
         'Expense Planner',
@@ -114,7 +115,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
     final txListWidget = Container(
       height:
-          (MediaQuery.of(context).size.height - appBar.preferredSize.height) *
+          (mediaQuery.size.height - appBar.preferredSize.height) *
               0.7,
       child: TransactionList(
         _userTransactions,
@@ -145,7 +146,7 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             if (!isLandscape)
               Container(
-                height: (MediaQuery.of(context).size.height -
+                height: (mediaQuery.size.height -
                         appBar.preferredSize.height) *
                     0.3,
                 child: Chart(_recentTransactions),
@@ -153,7 +154,7 @@ class _MyHomePageState extends State<MyHomePage> {
             if (!isLandscape) txListWidget,
             if (isLandscape) _showChart
                 ? Container(
-                    height: (MediaQuery.of(context).size.height -
+                    height: (mediaQuery.size.height -
                             appBar.preferredSize.height) *
                         0.6,
                     child: Chart(_recentTransactions),
