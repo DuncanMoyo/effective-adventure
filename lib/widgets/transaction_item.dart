6481a526e016951eb1fdd9be9aef22fa1a852087
Duncan Moyo <duncanfmoyo@gmail.com>
2021-05-20 22:45:42 +0200

    A problem with Lists and Stateful widgets

--- lib/widgets/transaction_item.dart
+++ lib/widgets/transaction_item.dart
@@ -1,9 +1,10 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import '../models/transaction.dart';
 
 import 'package:intl/intl.dart';
 
-class TransactionItem extends StatelessWidget {
+class TransactionItem extends StatefulWidget {
   const TransactionItem({
     Key key,
     @required this.transaction,
@@ -12,6 +13,27 @@ class TransactionItem extends StatelessWidget {
 
   final Transaction transaction;
   final Function deleteTransaction;
+
+  @override
+  _TransactionItemState createState() => _TransactionItemState();
+}
+
+class _TransactionItemState extends State<TransactionItem> {
+
+  Color _bgColor;
+
+  @override
+  void initState() {
+    const availableColors = [
+      Colors.red,
+      Colors.cyan,
+      Colors.orange,
+      Colors.limeAccent,
+    ];
+
+    _bgColor = availableColors[Random().nextInt(4)];
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,26 +45,26 @@ class TransactionItem extends StatelessWidget {
       ),
       child: ListTile(
         leading: CircleAvatar(
+          backgroundColor: _bgColor,
           radius: 30,
           child: Padding(
             padding: const EdgeInsets.all(6),
             child: FittedBox(
-              child: Text(
-                  'R: ${transaction.amount.toStringAsFixed(2)}'),
+              child: Text('R: ${widget.transaction.amount.toStringAsFixed(2)}'),
             ),
           ),
         ),
         title: Text(
-          transaction.title,
+          widget.transaction.title,
           style: Theme.of(context).textTheme.headline6,
         ),
         subtitle: Text(
-          DateFormat.yMMMMd().format(transaction.date),
+          DateFormat.yMMMMd().format(widget.transaction.date),
         ),
         trailing: MediaQuery.of(context).size.width > 460
             ? TextButton.icon(
                 onPressed: () =>
-                    deleteTransaction(transaction.id),
+                    widget.deleteTransaction(widget.transaction.id),
                 icon: Icon(
                   Icons.delete,
                   color: Theme.of(context).errorColor,
@@ -58,7 +80,7 @@ class TransactionItem extends StatelessWidget {
                 icon: const Icon(Icons.delete),
                 color: Theme.of(context).errorColor,
                 onPressed: () =>
-                    deleteTransaction(transaction.id),
+                    widget.deleteTransaction(widget.transaction.id),
               ),
       ),
     );
