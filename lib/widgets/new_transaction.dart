71c51808d4e961540db411ff9d51381c20feaaaf
Duncan Moyo <duncanfmoyo@gmail.com>
2021-05-20 11:57:18 +0200

    creating custom adaptive widgets

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,5 +1,4 @@
-import 'dart:io';
-
+import 'package:expense_planner/widgets/adaptive_text_button.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
@@ -96,25 +95,7 @@ class _NewTransactionState extends State<NewTransaction> {
                               )}',
                       ),
                     ),
-                    Platform.isIOS
-                        ? CupertinoButton(
-                            child: Text(
-                              'Choose Date',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            onPressed: _presentDatePicker,
-                          )
-                        : TextButton(
-                            onPressed: _presentDatePicker,
-                            child: Text(
-                              'Choose Date',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          )
+                    AdaptiveTextButton('Choose Date', _presentDatePicker)
                   ],
                 ),
               ),
